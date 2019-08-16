48a26ae2f5b26eb49a27feb61ad82b1debe9ea47
helloqingfeng <1287869707@qq.com>
2019-08-17 01:22:54 +0800

    修改 api

---
+++ build/precache-manifest.49e880ef4dc4caac113de975873aa5fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c801ae9c4ab1eb7154752a0fa2c25c5",
+    "url": "/Twice-demo/build/index.html"
+  },
+  {
+    "revision": "23b982ed8461126029c7",
+    "url": "/Twice-demo/build/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "ef96b28858cedd8e07d3",
+    "url": "/Twice-demo/build/static/css/main.8797b8f7.chunk.css"
+  },
+  {
+    "revision": "23b982ed8461126029c7",
+    "url": "/Twice-demo/build/static/js/2.b7dd699d.chunk.js"
+  },
+  {
+    "revision": "ef96b28858cedd8e07d3",
+    "url": "/Twice-demo/build/static/js/main.8496c41b.chunk.js"
+  },
+  {
+    "revision": "231c6b1764b08104af1c",
+    "url": "/Twice-demo/build/static/js/runtime~main.0ca381da.js"
+  }
+]);
